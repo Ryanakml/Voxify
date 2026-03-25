7dc03a5226a7c8724380ffd79d11bdffca59e89f
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2026-03-26 04:17:19 +0700

    feat(initial): setup next js, shadcn, clerk and prisma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -23,11 +31,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/proxy.ts
@@ -0,0 +1,49 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/(api|trpc)(.*)",
+]);
+
+const isOrgSelectionRoute = createRouteMatcher(["/org-selection(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, orgId } = await auth();
+
+  // allow public routes
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // protect private routes (non-publics)
+  if (!userId) {
+    await auth.protect();
+  }
+
+  // allow org selection pages
+  if (isOrgSelectionRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // allow org selection routes, ensure org is selected
+  if (isOrgSelectionRoute(req)) {
+    if (userId && !orgId) {
+      const orgSelection = new URL("/org-selection", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
